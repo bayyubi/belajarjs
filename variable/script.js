688f666e97fb4dd6a28ff78160bc73ecbfcf05eb
bayyubi <ubayrafif.js2@gmail.com>
2026-08-28 08:38:23 +0700

    Tugas 2

--- variable/script.js
+++ variable/script.js
@@ -1,4 +1,18 @@
 let nama = "Ucup Surucup";
 console.log(nama);
-nama = "Otong Surotong";
+
+nama = "Acong";
 console.log(nama);
+
+var nama_Depan = "Hideng";
+console.log(nama_Depan);
+namaDepan = "Ancol";
+console.log(namaDepan);
+
+let namaBelakang = "Ireng";
+{
+    let nama_Belakang = "Otong";
+    console.log(nama_Belakang);
+}
+
+console.log(namaBelakang);

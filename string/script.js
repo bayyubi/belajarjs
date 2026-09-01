2e2bde06c1b2aa40411ed5473180a3b1d8eb0f5b
bayyubi <ubayrafif.js2@gmail.com>
2026-09-01 14:14:14 +0700

    Tugas 4

---
+++ string/script.js
@@ -0,0 +1,8 @@
+let dataString = "data string";
+console.log(dataString);
+
+//1. Escaping String
+let data1 = 'Ubay berkata "Apa Kabar dunia?"';
+console.log(data1);
+let data2 = "bayu berkata \"Tidak baik-baik saja\"";
+console.log(data2);

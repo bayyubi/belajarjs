5d02478a36436a6bf89baba05f18c00c16289481
bayyubi <ubayrafif.js2@gmail.com>
2026-09-01 13:31:14 +0700

    Tugas 4

---
+++ tipedata/script.js
@@ -0,0 +1,15 @@
+let dataString = "Ubay"; //?String
+console.log(dataString);
+console.log(typeof dataString);
+
+let dataNumber = 98; //?Number
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+let dataBoolean = true; //?Boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+let dataKosong; //?Undefined
+console.log(dataKosong);
+console.log(typeof dataKosong);

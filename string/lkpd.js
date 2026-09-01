be2c5b012bd46d44ae6b47972ff62f6a7e8ef079
bayyubi <ubayrafif.js2@gmail.com>
2026-09-01 14:53:24 +0700

    Tugas 4

---
+++ string/lkpd.js
@@ -0,0 +1,27 @@
+//! Langkah 2 - Menulis String Literal
+
+//let a = 'Belajar Javascript itu menyenangkan.';
+//let b = "Belajar Javascript itu menyenangkan.";
+//let c = `Belajar Javascript itu menyenangkan.`;
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+//! Langkah 3 - Menulis String dengan Tanda Kutip di dalamnya
+
+//let kalimat1 = 'Guru berkata: \'Kerjakan tugas dengan teliti\'';
+//let kalimat2 = "Guru berkata: \"Kerjakan tugas dengan teliti\"";
+//console.log(kalimat1);
+//console.log(kalimat2);
+
+//! Langkah 4 - Menggunakan Escape \n dan \t
+
+//let biodata = 'Nama:\tAndi Saputra\nKelas:\tXII RPL\nSekolah:\tSMK Harapan Bangsa';
+//console.log(biodata);
+
+//! Langkah 5 - Membandingkan dengan Template Literal
+
+//let nama = 'Andi Saputra';
+//let kelas = 'XII RPL 1';
+//let biodataTL = `Nama: ${nama} Kelas: ${kelas} Sekolah: SMK Harapan Bangsa`;
+//console.log(biodataTL);

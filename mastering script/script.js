e235845c425c0d5e4291ebad2f4b76106546a1e7
bayyubi <ubayrafif.js2@gmail.com>
2026-09-02 13:46:17 +0700

    Tugas 5

---
+++ mastering script/script.js
@@ -0,0 +1,12 @@
+let data1 = 'Ubay \tberkata "Apa Kabar dunia?"';
+console.log(data1);
+let data2 = "Bayu berkata \"Tidak baik-baik saja\"";
+console.log(data2);
+let data3 = "Ubay berjalan-jalan di tepi pantai, \nkereeen";
+console.log(data3);
+
+let namaDepan = "Ubay";
+let namaBelakang = "Rafi";
+let umur = 7;
+let namaLengkap = umur + " " + namaDepan + " " + namaBelakang;
+console.log(namaLengkap);

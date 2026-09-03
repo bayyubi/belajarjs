05ba7d1d196af6495c555bdba899c8f2369368a0
bayyubi <ubayrafif.js2@gmail.com>
2026-09-03 14:45:41 +0700

    Tugas 5

--- operasi string/script.js
+++ operasi string/script.js
@@ -1,4 +1,4 @@
-//! Operasi String
+//! charAt
 let kata = "ABCDEF";
 console.log(kata.charAt(0)); //? A
 console.log(kata.charAt(1)); //? B
@@ -8,20 +8,20 @@ console.log(kata.charAt(4)); //? E
 console.log(kata.charAt(5)); //? F
 console.log(kata.charAt(6)); //? Undefined
 
-//! Mencari posisi karakter
+//! IndexOf
 let namaLengkap = "Bayu Yuba";
 console.log(namaLengkap.indexOf("B")); //? 0
 console.log(namaLengkap.indexOf("y")); //? 3
 console.log(namaLengkap.indexOf("u")); //? 2
 console.log(namaLengkap.indexOf("a")); //? 7
 
-//! Menggabungkan Kata
+//! Concat
 
 let namaDepan = "Bayu";
 let namaBelakang = "Yuba";
 console.log(namaDepan.concat(" ", namaBelakang)); //? Bayu Yuba
 
-//! Memotong Kata
+//! Substring
 
 let kata1 = "Belajar JavaScript";
 console.log(kata1.substring(5, 12)); //? ar Java
@@ -29,3 +29,8 @@ console.log(kata1.substring(5, 12)); //? ar Java
 //! Slice
 let kata2 = "Bayu Belajar JavaScript";
 console.log(kata2.slice(5, 12)); //? Belajar
+
+//! Replace
+
+let nama = "Budi Yuba";
+console.log(nama.replace("Budi", "Bayu")); //? Bayu Yuba

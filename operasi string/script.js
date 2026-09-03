8b32c67ae90cd4880da7d5b433d67184f8c8c852
bayyubi <ubayrafif.js2@gmail.com>
2026-09-03 13:56:06 +0700

    Tugas 7

--- operasi string/script.js
+++ operasi string/script.js
@@ -1,4 +1,4 @@
-// Operasi String
+//! Operasi String
 let kata = "ABCDEF";
 console.log(kata.charAt(0)); //? A
 console.log(kata.charAt(1)); //? B
@@ -7,3 +7,16 @@ console.log(kata.charAt(3)); //? D
 console.log(kata.charAt(4)); //? E
 console.log(kata.charAt(5)); //? F
 console.log(kata.charAt(6)); //? Undefined
+
+//! Mencari posisi karakter
+let namaLengkap = "Bayu Yuba";
+console.log(namaLengkap.indexOf("B")); //? 0
+console.log(namaLengkap.indexOf("y")); //? 3
+console.log(namaLengkap.indexOf("u")); //? 2
+console.log(namaLengkap.indexOf("a")); //? 7
+
+//! Menggabungkan Kata
+
+let namaDepan = "Bayu";
+let namaBelakang = "Yuba";
+console.log(namaDepan.concat(" ", namaBelakang)); //? Bayu Yuba

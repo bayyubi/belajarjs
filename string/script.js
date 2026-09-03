bcadee90e5dbdf4ec6cce37ba60c58b7bc86a73c
bayyubi <ubayrafif.js2@gmail.com>
2026-09-03 13:21:34 +0700

    Tugas 5

--- string/script.js
+++ string/script.js
@@ -1,7 +1,7 @@
 let dataString = "data string";
 console.log(dataString);
 
-//1. Escaping String
+//? 1. Escaping String
 let data1 = 'Ubay berkata "Apa Kabar dunia?"';
 console.log(data1);
 let data2 = "bayu berkata \"Tidak baik-baik saja\"";

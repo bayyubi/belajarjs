0d3a307058d28768e870b3dd72ed27cf9a135024
bayyubi <ubayrafif.js2@gmail.com>
2026-09-03 13:50:06 +0700

    Tugas 7

---
+++ operasi string/script.js
@@ -0,0 +1,9 @@
+// Operasi String
+let kata = "ABCDEF";
+console.log(kata.charAt(0)); //? A
+console.log(kata.charAt(1)); //? B
+console.log(kata.charAt(2)); //? C
+console.log(kata.charAt(3)); //? D
+console.log(kata.charAt(4)); //? E
+console.log(kata.charAt(5)); //? F
+console.log(kata.charAt(6)); //? Undefined

4def5196921bf055bb5945f93433166bb073e973
bayyubi <ubayrafif.js2@gmail.com>
2026-09-04 08:29:20 +0700

    Tugas 8

--- manipulasi string 2/script.js
+++ manipulasi string 2/script.js
@@ -5,3 +5,19 @@ console.log(nama1.toLowerCase()); //? belajar javascript
 //! uppercase
 nama = "BELAJAR javascript";
 console.log(nama.toUpperCase()); //? BELAJAR JAVASCRIPT 
+
+//! ParseInt
+let angka = "100.5";
+console.log(parseInt(angka)); //? 100
+
+let a = "20";
+let b = parseInt(a);
+console.log(b+10);
+
+//! ParseFloat
+angka = "100.5";
+console.log(parseFloat(angka)); //? 100.5
+
+let c = "20.5"
+let d = "10.5"
+console.log(parseFloat(c) + parseFloat(d)); //? 31

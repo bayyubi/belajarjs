98c25887a4cc62ba03c6e22adf411542eb003031
bayyubi <ubayrafif.js2@gmail.com>
2026-09-04 09:29:24 +0700

    Tugas 8

---
+++ manipulasi string 2/lkpd.js
@@ -0,0 +1,41 @@
+//! KEGIATAN 1 - Memprediksi Output
+
+//console.log("Belajar" + " " + "JavaScript"); //? 1
+//console.log("Halo Dunia".length); //? 2
+//console.log("javascript".toUpperCase()); //? 3
+//console.log("  Coding Itu Seru  ".trim()); //? 4
+//let nama = "Rina"; console.log(`Halo, ${nama}!`); //? 5
+//console.log("kucing".indexOf("c")); //? 6
+//console.log("belajar".includes("jar")); //? 7
+//console.log("Selamat pagi".replace("pagi", "siang")); //? 8
+
+//! Kegiatan 2 — Melengkapi Kode
+
+// Tampilkan nama dalam huruf kecil semua //? 1
+//let nama = "BUDIMAN";
+//console.log(nama.toLowerCase());
+
+// Gabungkan dua kata menjadi satu kalimat sapaan //? 2
+//let sapaan = "Selamat";
+//let waktu = "Pagi";
+//let hasil = sapaan + " " + waktu;
+//console.log(hasil); // Selamat Pagi
+
+// Ambil 3 karakter pertama dari kata 'JavaScript' //? 3
+//let bahasa = "JavaScript";
+//console.log(bahasa.substring(0, 3)); // Jav
+
+//! Kegiatan 3 — Studi Kasus
+
+//let nama = "Ubay"; //? 1
+//let pekerjaan = "Software Engineer";
+//let kartuNama = `Nama saya ${nama}, saya bekerja sebagai ${pekerjaan}`;
+//console.log(kartuNama);
+
+//let email1 = "ubay@gmail.com"; //? 2
+//let email2 = "bayugmail.com";
+//console.log(email1.includes("@")); //? true
+//console.log(email2.includes("@")); //? false
+
+//let kalimat = "I love JavaScript"; //? 3
+//console.log(kalimat.split(" ")); //? ["I", "love", "JavaScript"]

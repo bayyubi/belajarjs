95da544118a1fc52c8d50f8244b0d4c29c4cebc6
bayyubi <ubayrafif.js2@gmail.com>
2026-09-10 13:16:37 +0700

    Tugas 11

---
+++ anatomi number/pembeliandanpajak.js
@@ -0,0 +1,13 @@
+//! Error Logika string
+let pembelian1 = "10000";
+let pajak1 = 0.12;
+
+let bayar1 = pembelian1 + (pembelian1 * pajak1);
+console.log(bayar1);
+
+//! Kalkulasi Sukses
+let pembelian2 = "10000";
+let pajak2 = 0.12;
+
+let bayar2 = parseInt(pembelian2) + (parseInt(pembelian2) * pajak2);
+console.log(bayar2)
---
+++ anatomi number/script.js
@@ -0,0 +1,5 @@
+let angka1 = "17 ini umur saya";
+console.log(parseInt(angka1)); //? output 17
+
+let angka2 = "ini umur saya 17";
+console.log(parseInt(angka2)); //? output NaN

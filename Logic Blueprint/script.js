d6cfe3b6767d4e01ff66844c019e460e45189247
bayyubi <ubayrafif.js2@gmail.com>
2026-09-10 11:45:46 +0700

    draf

---
+++ Logic Blueprint/script.js
@@ -0,0 +1,6 @@
+function proses() {
+    const input1 = document.getElementById('input1').value;
+    const input2 = document.getElementById('input2').value;
+    const output = document.getElementById('output');
+    output.innerText = input1 + ' ' + input2;
+}

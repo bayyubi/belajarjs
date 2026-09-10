a14189aa0e4abaaa80efb1e0a56c891e14f94adb
bayyubi <ubayrafif.js2@gmail.com>
2026-09-10 13:19:52 +0700

    Tugas 11

--- anatomi number/pembeliandanpajak.js
+++ anatomi number/pembeliandanpajak.js
@@ -3,6 +3,7 @@ let pembelian1 = "10000";
 let pajak1 = 0.12;
 
 let bayar1 = pembelian1 + (pembelian1 * pajak1);
+console.log("Error Logika String");
 console.log(bayar1);
 
 //! Kalkulasi Sukses
@@ -10,4 +11,5 @@ let pembelian2 = "10000";
 let pajak2 = 0.12;
 
 let bayar2 = parseInt(pembelian2) + (parseInt(pembelian2) * pajak2);
+console.log("Kalkulasi Sukses");
 console.log(bayar2)

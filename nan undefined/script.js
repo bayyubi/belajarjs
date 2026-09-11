1447ba949c9d3ced6bb465fdb0f261c0e8776a26
bayyubi <ubayrafif.js2@gmail.com>
2026-09-11 08:29:49 +0700

    Tugas 12

---
+++ nan undefined/script.js
@@ -0,0 +1,14 @@
+//? Hantu 1 Matematika
+console.log('Bayu'/'Yuba'); //? NaN
+console.log('Bayu'-'Yuba'); //? NaN
+console.log('Bayu'*'Yuba'); //? NaN
+console.log('Bayu'+'Yuba');  //?
+
+
+//? Hantu Memori
+let a;
+console.log(a); //? Undefined
+
+let umur = "18 adalah umur saya";
+let angka = parseInt(umur);
+console.log(angka + 2) //? 20

2688d60fc6fb9edb6b9edf6e2350fab383e30a9e
bayyubi <ubayrafif.js2@gmail.com>
2026-09-11 08:16:14 +0700

    Tugas 11

---
+++ anatomi number/lkpd.js
@@ -0,0 +1,38 @@
+//! Kegiatan 2
+//let a = 8;
+//let b = 3;
+//console.log(a + b);   //? 11
+//console.log(a - b);   //? 5
+//console.log(a % b);   //? 2
+//console.log(a / 0);   //? Invinity
+//console.log(a ** 2);  //? 64
+//console.log(typeof a); //? number
+
+//! Kegiatan 3
+//let panjang = 5;    //? 1
+//let lebar = 10;
+
+//let luas = (panjang) * (lebar);
+//console.log(luas)
+
+//let suhu = "32 Celcius"; //? 2
+//let konversi = (parseInt(suhu) * 9/5) + 32;
+//console.log(konversi) 
+
+//let nilai = "12.75"; //? 3
+//console.log(parseInt(nilai));
+//console.log(parseFloat(nilai))
+
+//console.log(isNaN(123)); //? 4
+//console.log(isNaN("123)"))
+
+//let nilai = 78.3456; //? 5
+//console.log(nilai.toFixed(2));
+
+//! Kegiatan 4
+let harga = "15000 Rupiah";
+let jumlah = "7 barang";
+let diskon = 1;
+
+let bayar = parseInt(harga) * (parseInt(jumlah) * diskon);
+console.log(bayar)
--- anatomi number/script.js
+++ anatomi number/script.js
@@ -3,3 +3,6 @@ console.log(parseInt(angka1)); //? output 17
 
 let angka2 = "ini umur saya 17";
 console.log(parseInt(angka2)); //? output NaN
+
+let angka3 = 17;
+console.log(angka3 / 0); //? output Infinity

56327e79e57e3e5f853f3d43b103451cf5fa2577
bayyubi <ubayrafif.js2@gmail.com>
2026-09-14 08:46:18 +0700

    Tugas 12

---
+++ nan undefined/lkpd.js
@@ -0,0 +1,35 @@
+//! Kegiatan 1
+
+//let a;
+//console.log(a);
+
+//let b;
+//console.log(typeof b);
+
+//let hasil = "10" - "abc";
+//console.log(hasil);
+
+//let siswa = { nama: "Dewi" };
+//console.log(siswa.alamat);
+
+//function sapa() {
+//  console.log("Halo");
+//}   
+//console.log(sapa());
+
+//console.log(0 / 0);
+
+//console.log(10 / "lima");
+
+//console.log(NaN === NaN);
+
+//console.log(isNaN("halo"));
+
+//! Kegiatan 2
+
+let nilai1 = 80;
+let nilai2 = 90;
+let nilai3 = 75;
+
+let rerata = (nilai1 + nilai2 + nilai3) / 3;
+console.log("Rata-rata:", rerata);

6061c45f8a4c6b7f4704f4ec8f02f997f816252f
bayyubi <ubayrafif.js2@gmail.com>
2026-09-16 13:33:25 +0700

    Tugas 14

--- aritmatika2/script.js
+++ aritmatika2/script.js
@@ -1,1 +1,5 @@
-console.log("test");
+let dataBolean = (10>7); //? True, karena 10 lebih besar dari 7
+console.log(dataBolean)
+
+let data = 3=="3"; //? True, karena nilai integer dan string sama
+console.log(data);

4b852a293f0745609aeb36c89e1c9186acaf966c
bayyubi <ubayrafif.js2@gmail.com>
2026-09-16 13:47:09 +0700

    Tugas 14

--- aritmatika2/script.js
+++ aritmatika2/script.js
@@ -1,5 +1,14 @@
 let dataBolean = (10>7); //? True, karena 10 lebih besar dari 7
 console.log(dataBolean)
 
-let data = 3=="3"; //? True, karena nilai integer dan string sama
-console.log(data);
+let dataBoolean = 10<=10;
+console.log(dataBoolean);
+
+let data1 = 3=="3"; //? True, karena nilai integer dan string sama
+console.log(data1); //! Hindari praktik ini di kode modern! (Lose Equality)
+
+let data2 = 4==="4"; //! Strict Equality
+console.log(data2)
+
+let data3 = 5!=="5" //! Strict Inquality
+console.log(data3)

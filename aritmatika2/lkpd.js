e3cfd244a263f172c257ff2d55e707a513634260
bayyubi <ubayrafif.js2@gmail.com>
2026-09-17 13:29:03 +0700

    Tugas 14

---
+++ aritmatika2/lkpd.js
@@ -0,0 +1,9 @@
+let angka1 = document.getElementById('aritmatikaAngka1').valueAsNumber;
+
+if (hasil = angka1) {
+  tes = "tes";
+} else {
+  tes = "set";
+}
+
+document.getElementById('hasil').textContent = hasil;
